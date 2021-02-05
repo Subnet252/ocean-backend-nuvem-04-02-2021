729ef309bd0155167c02593f7a9a9e2831b46d06
Subnet 252 <subnet252@gmail.com>
2021-02-04 21:19:23 -0300

    Update index.js
    ocean_db name

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient, ObjectId } = require('mongodb');
 (async () => {
     const url = 'mongodb+srv://admin:Apinay33@cluster0.xqlxr.mongodb.net/ocean_db?retryWrites=true&w=majority';
 
-    const dbName = 'ocean_database_03_02_2021';
+    const dbName = 'ocean_db';
 
     console.info('Conectando ao banco de dados...');
 
